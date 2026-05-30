ee915167cdae71259cd60ccef5b618000680233b
SmartLin <linguangyan@cnzhengding.com>
2026-05-30 09:36:03 +0800

    新增 store/headers

--- background/express/api/mall/index.js
+++ background/express/api/mall/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { getFlatMallList } = require('~store/user')
+const { getFlatMallList, getMall } = require('~store/user')
 const { map } = require('lodash')
 const router = express.Router()
 
@@ -11,6 +11,23 @@ router.post('/store/list', async (req, res, next) => {
     return
   }
   res.customResult = [false, map(mallList, 'userInfo')]
+  next()
+})
+
+router.post('/store/headers', async (req, res, next) => {
+  const { mallId } = req.body
+  if (!mallId) {
+    res.customResult = [true, 'mallId 不能为空']
+    next()
+    return
+  }
+  const mall = getMall(mallId)
+  if (!mall) {
+    res.customResult = [true, '店铺不存在']
+    next()
+    return
+  }
+  res.customResult = [false, mall?.headers]
   next()
 })
 
